eefc5c1667a3a72a24bc0b5e1cff712d0e1af5b3
sebastianbarragan02@gmail.com <sebastian.barragan71@misena.edu.co>
2023-10-05 13:03:57 -0500

    Carrito de compras (clase 21)

--- js/main.js
+++ js/main.js
@@ -2,15 +2,47 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleAsideShoppingCart)
 
 function toggleDesktopMenu(){
+
+    const isAsideColsed = aside.classList.contains('inactive');
+
+    if (!isAsideColsed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+
+    const isAsideColsed = aside.classList.contains('inactive');
+
+    if (!isAsideColsed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideShoppingCart(){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMenuDesktopClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isMenuDesktopClosed) {
+        desktopMenu.classList.add('inactive');
+        
+    }
+
+    aside.classList.toggle('inactive');
 }
