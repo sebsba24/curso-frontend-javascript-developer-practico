d6cc6053b20729c930b111d5ebc0009260bdc9e9
sebastianbarragan02@gmail.com <sebastian.barragan71@misena.edu.co>
2023-10-09 13:00:18 -0500

    Interaccion entre todos los componentes (Clase 24)

--- js/main.js
+++ js/main.js
@@ -4,14 +4,18 @@ const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
-menuShoppingCart.addEventListener('click', toggleAsideShoppingCart)
+menuShoppingCart.addEventListener('click', toggleAsideShoppingCart);
+productDetailCloseIcon.addEventListener('click', closeProductDetail);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
 
     const isAsideColsed = shoppingCartContainer.classList.contains('inactive');
 
@@ -28,13 +32,17 @@ function toggleMobileMenu() {
     if (!isAsideColsed) {
         shoppingCartContainer.classList.add('inactive');
     }
+
+    closeProductDetail();
+
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleAsideShoppingCart(){
+function toggleAsideShoppingCart() {
 
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isMenuDesktopClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
 
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
@@ -42,10 +50,27 @@ function toggleAsideShoppingCart(){
 
     if (!isMenuDesktopClosed) {
         desktopMenu.classList.add('inactive');
-        
+
     }
 
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+
+
     shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetail() {
+
+    shoppingCartContainer.classList.add('inactive');
+
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetail() {
+    productDetailContainer.classList.add('inactive');
 }
 
 
@@ -72,43 +97,44 @@ productList.push({
     info: 'Your flower will be so comfortable'
 });
 
-function renderProducts(arr){
+function renderProducts(arr) {
 
-    for (product of arr){
+    for (product of arr) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
+
         // * product = {name, price, image} -> prouct.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-    
+        productImg.addEventListener('click', openProductDetail);
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-    
+
         const productInfoDiv = document.createElement('div');
-    
+
         const productPrice = document.createElement('p');
         productPrice.innerText = '$' + product.price;
         const productName = document.createElement('p');
         productName.innerText = product.name;
-    
+
         productInfoDiv.appendChild(productPrice);
         productInfoDiv.appendChild(productName);
-    
+
         const productInfoFigure = document.createElement('figure');
         const productImgCart = document.createElement('img');
         productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-    
+
         productInfoFigure.appendChild(productImgCart);
-    
+
         productInfo.appendChild(productInfoDiv);
         productInfo.appendChild(productInfoFigure);
-    
+
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
-    
+
         cardsContainer.appendChild(productCard);
-    
+
     }
 }
 
